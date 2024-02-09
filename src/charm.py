cbff2f14878ef2134aa2cb20fd4adfceeacf279a
Ghislain Bourgeois <ghislain.bourgeois@canonical.com>
2024-02-09 10:45:08 -0500

    feat: Add provides relation and implement proxying of requests to our provider (#8)

--- src/charm.py
+++ src/charm.py
@@ -8,35 +8,205 @@ Certificates are provided by the operator trough Juju configs.
 """
 
 import logging
+from typing import Optional
 
-from ops.charm import CharmBase, InstallEvent
+from charms.tls_certificates_interface.v3.tls_certificates import (  # type: ignore[import-not-found]  # noqa: E501
+    AllCertificatesInvalidatedEvent,
+    CertificateAvailableEvent,
+    CertificateCreationRequestEvent,
+    CertificateInvalidatedEvent,
+    CertificateRevocationRequestEvent,
+    TLSCertificatesProvidesV3,
+    TLSCertificatesRequiresV3,
+)
+from ops.charm import CharmBase, EventBase
 from ops.main import main
-from ops.model import ActiveStatus
+from ops.model import ActiveStatus, BlockedStatus
 
 logger = logging.getLogger(__name__)
+
+PROVIDES_RELATION_NAME = "certificates-provides"
+REQUIRES_RELATION_NAME = "certificates-requires"
 
 
 class TLSConstraintsCharm(CharmBase):
     """Main class to handle Juju events."""
 
     def __init__(self, *args):
-        """Observes config change and certificate request events."""
+        """Setup charm integration handlers and observe Juju events."""
         super().__init__(*args)
-        self.framework.observe(self.on.install, self._on_install)
+        self.certificates_provider = TLSCertificatesRequiresV3(self, REQUIRES_RELATION_NAME)
+        self.certificates_requirers = TLSCertificatesProvidesV3(self, PROVIDES_RELATION_NAME)
+        self.framework.observe(self.on.install, self._update_status)
+        self.framework.observe(self.on.update_status, self._update_status)
+        self.framework.observe(self.on.certificates_requires_relation_joined, self._update_status)
+        self.framework.observe(self.on.certificates_provides_relation_joined, self._update_status)
+        self.framework.observe(
+            self.certificates_requirers.on.certificate_creation_request,
+            self._on_certificate_creation_request,
+        )
+        self.framework.observe(
+            self.certificates_requirers.on.certificate_revocation_request,
+            self._on_certificate_revocation_request,
+        )
+        self.framework.observe(
+            self.certificates_provider.on.certificate_available,
+            self._on_certificate_available,
+        )
+        self.framework.observe(
+            self.certificates_provider.on.certificate_invalidated,
+            self._on_certificate_invalidated,
+        )
+        self.framework.observe(
+            self.certificates_provider.on.all_certificates_invalidated,
+            self._on_all_certificates_invalidated,
+        )
 
-    def _on_install(self, event: InstallEvent) -> None:
-        """Handles the install event.
+    def _update_status(self, event: EventBase) -> None:
+        """Handles charm events that need to update the status.
 
-        The charm will be in Active Status and ready to handle actions.
+        The charm will be in Active Status when related to a TLS Provider
+        and Blocked status otherwise.
 
         Args:
-            event (InstallEvent): Juju event.
+            event (EventBase): Juju event.
 
         Returns:
             None
         """
+        if not self.model.get_relation(REQUIRES_RELATION_NAME):
+            self.unit.status = BlockedStatus("Need a relation to a TLS certificates provider")
+            return
         self.unit.status = ActiveStatus()
+
+    def _on_certificate_creation_request(self, event: CertificateCreationRequestEvent) -> None:
+        """Handles certificate creation request events.
+
+        If a TLS provider is not integrated to this charm, the event will be
+        defered and the status will be Blocked.
+        Otherwise, the request will be forwarded to the provider.
+
+        Args:
+            event (CertificateCreationRequestEvent): Event containing the request
+
+        Returns:
+            None
+        """
+        if not self.model.get_relation(REQUIRES_RELATION_NAME):
+            event.defer()
+            self.unit.status = BlockedStatus("Need a relation to a TLS certificates provider")
+            return
+        self.certificates_provider.request_certificate_creation(
+            event.certificate_signing_request.encode(), event.is_ca
+        )
+
+    def _on_certificate_revocation_request(self, event: CertificateRevocationRequestEvent) -> None:
+        """Handles certificate revocation request events.
+
+        In the unlikely case a TLS provider is not integrated to this charm,
+        the status will be blocked, and this event will be ignored.
+        Otherwise, forward the revocation request to the provider.
+
+        Args:
+            event (CertificateRevocationRequestEvent): Event containing the request
+
+        Returns:
+            None
+        """
+        if not self.model.get_relation(REQUIRES_RELATION_NAME):
+            self.unit.status = BlockedStatus("Need a relation to a TLS certificates provider")
+            return
+        self.certificates_provider.request_certificate_revocation(
+            event.certificate_signing_request.encode()
+        )
+
+    def _on_certificate_available(self, event: CertificateAvailableEvent) -> None:
+        """Handles certificate available events.
+
+        Finds the relation ID matching the CSR and forwards the received
+        certificate to that relation.
+        If a relation ID is not found, logs an error and ignores the event.
+
+        Args:
+            event (CertificateAvailableEvent): Event containing the certificate
+
+        Returns:
+            None
+        """
+        relation_id = self._get_relation_id_for_csr(event.certificate_signing_request)
+        if not relation_id:
+            logger.error(
+                "Could not find the relation for CSR: %s.",
+                event.certificate_signing_request,
+            )
+            return
+        self.certificates_requirers.set_relation_certificate(
+            certificate=event.certificate,
+            certificate_signing_request=event.certificate_signing_request,
+            ca=event.ca,
+            chain=event.chain,
+            relation_id=relation_id,
+        )
+
+    def _on_certificate_invalidated(self, event: CertificateInvalidatedEvent) -> None:
+        """Handles certificate invalidated events.
+
+        If the certificate is invalidated because it expired, ignore the event
+        and let the requirer handle it properly. Otherwise, calls the TLS
+        library to revoke the certificate to the requirer.
+
+        Args:
+            event (CertificateInvalidatedEvent): Event for invalidated certificate
+
+        Returns:
+            None
+        """
+        if event.reason == "expired":
+            return
+        self.certificates_requirers.remove_certificate(event.certificate)
+
+    def _on_all_certificates_invalidated(self, event: AllCertificatesInvalidatedEvent) -> None:
+        """Handles all certificates invalidated events.
+
+        Revokes all certificates.
+
+        Args:
+            event (AllCertificatesInvalidatedEvent): Event for all certificates invalidated
+
+        Returns:
+            None
+        """
+        self.certificates_requirers.revoke_all_certificates()
+
+    def _get_relation_id_for_csr(self, csr: str) -> Optional[int]:
+        """Finds the relation ID that sent the provided CSR.
+
+        This should return a single relation ID, otherwise it means multiple
+        applications requested the same certificate using the same private
+        key. In that case, we log an error and return None.
+
+        Args:
+            csr (str): Certificate Signing Request to search
+
+        Returns:
+            Relation ID (int) or None
+        """
+        all_requirers_csrs = self.certificates_requirers.get_requirer_csrs()
+        relation_ids = {
+            requirer_csr.relation_id
+            for requirer_csr in all_requirers_csrs
+            if requirer_csr.csr == csr
+        }
+        if not relation_ids:
+            return None
+        if len(relation_ids) > 1:
+            logger.error(
+                "Multiple requirers have the same CSR. Cannot choose one between relation IDs: %s",  # noqa: E501
+                relation_ids,
+            )
+            return None
+        return relation_ids.pop()
 
 
 if __name__ == "__main__":
-    main(TLSConstraintsCharm)
+    main(TLSConstraintsCharm)  # pragma: nocover
--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -1,42 +1,133 @@
 # Copyright 2024 Canonical Ltd.
 # See LICENSE file for licensing details.
 
+import asyncio
 import logging
+from pathlib import Path
 
 import pytest
-from juju.errors import JujuError
+import yaml
 from pytest_operator.plugin import OpsTest
 
 logger = logging.getLogger(__name__)
 
-APPLICATION_NAME = "tls-constraints"
+METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
+APPLICATION_NAME = METADATA["name"]
+
+TLS_PROVIDER_CHARM_NAME = "self-signed-certificates"
+TLS_REQUIRER_CHARM_NAME = "tls-certificates-requirer"
+TLS_REQUIRER1 = f"{TLS_REQUIRER_CHARM_NAME}1"
+TLS_REQUIRER2 = f"{TLS_REQUIRER_CHARM_NAME}2"
 
 
-class TestTLSConstraintsOperator:
-    @pytest.fixture(scope="module")
-    @pytest.mark.abort_on_fail
-    async def charm(self, ops_test):
-        ops_test.destructive_mode = False
-        charm = await ops_test.build_charm(".")
-        return charm
+@pytest.fixture(scope="module")
+@pytest.mark.abort_on_fail
+async def build_and_deploy(ops_test: OpsTest):
+    """Build the charm-under-test and deploy it."""
+    assert ops_test.model
+    charm_build = asyncio.create_task(ops_test.build_charm("."))
+    await ops_test.model.deploy(
+        TLS_PROVIDER_CHARM_NAME,
+        application_name=TLS_PROVIDER_CHARM_NAME,
+        channel="edge",
+    )
+    await ops_test.model.deploy(
+        TLS_REQUIRER_CHARM_NAME,
+        application_name=TLS_REQUIRER1,
+        channel="edge",
+    )
+    await ops_test.model.deploy(
+        TLS_REQUIRER_CHARM_NAME,
+        application_name=TLS_REQUIRER2,
+        channel="edge",
+    )
+    charm = await charm_build
+    await ops_test.model.deploy(
+        charm,
+        application_name=APPLICATION_NAME,
+        series="jammy",
+        trust=True,
+    )
 
-    @pytest.fixture()
-    async def cleanup(self, ops_test):
-        try:
-            await ops_test.model.remove_application(
-                app_name=APPLICATION_NAME, block_until_done=True
-            )
-        except JujuError:
-            pass
 
-    async def test_given_charm_when_deploy_then_status_is_active(
-        self, ops_test: OpsTest, charm, cleanup
-    ):
-        assert ops_test.model
-        await ops_test.model.deploy(
-            entity_url=charm,
-            application_name=APPLICATION_NAME,
-            series="jammy",
-        )
+async def test_given_charm_is_not_related_to_provider_when_deploy_then_status_is_blocked(
+    ops_test: OpsTest,
+    build_and_deploy,
+):
+    assert ops_test.model
+    await ops_test.model.wait_for_idle(apps=[APPLICATION_NAME], status="blocked", timeout=1000)
 
-        await ops_test.model.wait_for_idle(apps=[APPLICATION_NAME], status="active", timeout=1000)
+
+async def test_given_provider_is_related_then_status_is_active(
+    ops_test: OpsTest,
+    build_and_deploy,
+):
+    assert ops_test.model
+    await ops_test.model.add_relation(
+        relation1=f"{APPLICATION_NAME}:certificates-requires",
+        relation2=TLS_PROVIDER_CHARM_NAME,
+    )
+
+    await ops_test.model.wait_for_idle(apps=[APPLICATION_NAME], status="active", timeout=1000)
+
+
+async def test_given_tls_requirer1_is_deployed_and_related_then_certificate_is_created_and_passed_correctly(  # noqa: E501
+    ops_test: OpsTest,
+    build_and_deploy,
+):
+    assert ops_test.model
+    await ops_test.model.add_relation(
+        relation1=f"{APPLICATION_NAME}:certificates-provides", relation2=TLS_REQUIRER1
+    )
+    await ops_test.model.wait_for_idle(
+        apps=[TLS_REQUIRER1],
+        status="active",
+        timeout=1000,
+    )
+    action_output = await run_get_certificate_action(ops_test, TLS_REQUIRER1)
+    assert action_output["certificate"] is not None
+    assert action_output["ca-certificate"] is not None
+    assert action_output["csr"] is not None
+
+
+async def test_given_tls_requirer2_is_deployed_and_related_then_certificate_is_created_passed_correctly_and_different(  # noqa: E501
+    ops_test: OpsTest,
+    build_and_deploy,
+):
+    assert ops_test.model
+    await ops_test.model.add_relation(
+        relation1=f"{APPLICATION_NAME}:certificates-provides",
+        relation2=TLS_REQUIRER2,
+    )
+    await ops_test.model.wait_for_idle(
+        apps=[TLS_REQUIRER2],
+        status="active",
+        timeout=1000,
+    )
+    action_output2 = await run_get_certificate_action(ops_test, TLS_REQUIRER2)
+    assert action_output2["certificate"] is not None
+    assert action_output2["ca-certificate"] is not None
+    assert action_output2["csr"] is not None
+
+    action_output1 = await run_get_certificate_action(ops_test, TLS_REQUIRER1)
+    assert action_output1["certificate"] != action_output2["certificate"]
+    assert action_output1["csr"] != action_output2["csr"]
+
+
+async def run_get_certificate_action(ops_test: OpsTest, app_name: str) -> dict:
+    """Runs `get-certificate` on the first unit of app_name.
+
+    Args:
+        ops_test (OpsTest): OpsTest
+        app_name (str): Application Name to target
+
+    Returns:
+        dict: Action output
+    """
+    assert ops_test.model
+    tls_requirer_unit = ops_test.model.units[f"{app_name}/0"]
+    action = await tls_requirer_unit.run_action(
+        action_name="get-certificate",
+    )
+    action_output = await ops_test.model.get_action_output(action_uuid=action.entity_id, wait=240)
+    return action_output
