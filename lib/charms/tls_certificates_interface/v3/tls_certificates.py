d36f6e52776cb0c30788398ae929cf2b12606e8e
Daniel Arndt <daniel.arndt@canonical.com>
2024-12-13 14:35:00 -0400

    build: Enable pyright to check if type hints are missing
    This change adds a pyright check to ensure that we add type hints to our code

--- lib/charms/tls_certificates_interface/v3/tls_certificates.py
+++ lib/charms/tls_certificates_interface/v3/tls_certificates.py
@@ -526,7 +526,7 @@ class CertificateAvailableEvent(EventBase):
 class CertificateExpiringEvent(EventBase):
     """Charm Event triggered when a TLS certificate is almost expired."""
 
-    def __init__(self, handle, certificate: str, expiry: str):
+    def __init__(self, handle: Handle, certificate: str, expiry: str):
         """CertificateExpiringEvent.
 
         Args:
